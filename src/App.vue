1163fa0eb0f7888c83844421f96045f7089b5e94
jahid <JahidChetti@ugent.be>
2022-04-10 22:32:19 +0200

    simple redirect test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
 <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   <h1>{{message}}</h1>
+<!--  <SideBar></SideBar>-->
+  <component :is="currentView"/>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import SideBar from './components/Sidebar'
+import HelloWorld from "@/components/HelloWorld";
+
+const routes = {
+  '/': SideBar,
+  '/test': HelloWorld
+}
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    SideBar,
+    HelloWorld
   },
   data(){
     return{
-      message: "deez nuts"
+      message: "deez nuts",
+      currentPath: window.location.hash
     }
+  },
+  computed: {
+    currentView(){
+      return routes[this.currentPath.slice(1) || '/']
+    }
+  },
+  mounted(){
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+    })
   }
 }
 </script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <a href="#/test">Potato</a>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  data(){
+    return{}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
