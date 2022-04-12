c3e2dda716bdc64fa9e6fd149b90c9473ea332d4
jahid <JahidChetti@ugent.be>
2022-04-12 02:40:19 +0200

    #2

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -4,14 +4,14 @@
   <div class="topnav">
     <div v-if="!isFetching">
       <router-link to="/">Home</router-link>
-      <router-link :to="{name: 'books', params: {link: links['audiobooks'], users: links['users']}}">All books</router-link>
-      <router-link :to="{name: 'genres', params: {link: links['genres']}}">All genres</router-link>
+      <router-link :to="{name: 'books', params: {link: links['audiobooks'], users: links['users'], genresLink: links.genres}}">All books</router-link>
+      <router-link :to="{name: 'genres', params: {link: links['genres'], books: links['audiobooks']}}">All genres</router-link>
       <router-link :to="{name: 'users', params: {link: links['users']}}">All Users</router-link>
 
       <router-link :to="{name: 'bookForm', params: {title: 'Create Book', request: 'POST', link: links.audiobooks,
         redirectLink: links.audiobooks, route:'books', genresLink: links.genres}}">Create Book</router-link>
       <router-link :to="{name: 'genreForm', params: {title: 'Create Genre', request: 'POST', link: links.genres,
-            redirectRoute: 'genres', redirectUrl: links.genres}}">Create Genre</router-link>
+            redirectRoute: 'genres', redirectUrl: links.genres, books:links.audiobooks}}">Create Genre</router-link>
       <router-link :to="{name: 'userForm', params: {title: 'Create User', request: 'POST', link: links.users,
       redirectLink: links.users, route:'users'}}">Create User</router-link>
     </div>
